e55da1db09ccc01b1eb784f08a724a6e1dacef83
zack91344 - Zack Avitan <zack91344@outlook.com>
2014-08-20 13:45:45 -0700

    initial version of windows phone app

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Hello_World.Resources;
+
+namespace Hello_World
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
